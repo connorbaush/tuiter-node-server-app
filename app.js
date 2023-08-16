a6152670b4443217f7712f32ac7c03a1004afcea
connorbaush <baush.c@northeastern.edu>
2023-08-15 23:41:13 -0400

    quick change

--- app.js
+++ app.js
@@ -7,14 +7,13 @@ import TuitsController from "./controllers/tuits/tuits-controller.js";
 import session from "express-session";
 import AuthController from "./users/auth-controller.js";
 import mongoose from "mongoose";
-const CONNECTION_STRING = 'mongodb://127.0.0.1:27017/tuiter'
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter'
 mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(cors({
     credentials: true,
-    //origin: process.env.FRONTEND_URL
-    origin: 'http://localhost:3000'
+    origin: process.env.FRONTEND_URL
 }
 ))
 const sessionOptions = {
