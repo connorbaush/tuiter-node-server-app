8e95b88ff29f98181dc02e6409c128c9c939bba1
connorbaush <baush.c@northeastern.edu>
2023-08-15 23:40:03 -0400

    quick change

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors({
     credentials: true,
-    origin: process.env.FRONTEND_URL
+    //origin: process.env.FRONTEND_URL
+    origin: 'http://localhost:3000'
 }
 ))
 const sessionOptions = {
--- controllers/hello-controller.js
+++ controllers/hello-controller.js
@@ -3,7 +3,7 @@ const HelloController = (app) => {
         res.send('Life is good!')
     })
     app.get('/', (req, res) => {
-        res.send('Welcome to Full Stack Development!')
+        res.send('Welcome to Full Stack Development here's a change!')
     })
 }
 export default HelloController;
